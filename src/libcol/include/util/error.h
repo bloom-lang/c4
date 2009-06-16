97c6f860960c420461e72ae8fedc5163bb8d3176
neilc@samurai.com <none@none>
2009-06-15 21:15:43 -0700

    Bug fix; add initial code for printing backtraces.

--- src/libcol/include/util/error.h
+++ src/libcol/include/util/error.h
@@ -2,8 +2,8 @@
 #define ERROR_H
 
 #define FAIL()          fatal_error(__FILE__, __LINE__)
-#define ASSERT(cond)    (!(cond) || assert_fail(APR_STRINGIFY(cond), \
-                                                __FILE__, __LINE__))
+#define ASSERT(cond)    ((cond) || assert_fail(APR_STRINGIFY(cond),     \
+                                               __FILE__, __LINE__))
 
 void fatal_error(const char *file, int line_num);
 int assert_fail(const char *cond, const char *file, int line_num);
--- src/libcol/util/error.c
+++ src/libcol/util/error.c
@@ -1,11 +1,20 @@
+#include <stdlib.h>
+
+/* XXX: __MACH__ is probably slightly wrong */
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__MACH__)
+#define HAVE_EXECINFO
+#include <execinfo.h>
+#endif
+
 #include "col-internal.h"
 
-#include <stdlib.h>
+static void print_backtrace();
 
 void
 fatal_error(const char *file, int line_num)
 {
     fprintf(stderr, "FATAL ERROR at %s:%d\n", file, line_num);
+    print_backtrace();
     fflush(stderr);
     abort();
 }
@@ -15,8 +24,28 @@ assert_fail(const char *cond, const char *file, int line_num)
 {
     fprintf(stderr, "ASSERT FAILED: \"%s\", at %s:%d\n",
             cond, file, line_num);
+    print_backtrace();
     fflush(stderr);
     abort();
     return 0;   /* Keep the compiler quiet */
 }
 
+static void
+print_backtrace()
+{
+#ifdef HAVE_EXECINFO
+    void *bt_array[128];
+    char **bt_strings;
+    apr_size_t nframes;
+    apr_size_t i;
+
+    nframes = backtrace(bt_array, 128);
+    bt_strings = backtrace_symbols(bt_array, nframes);
+
+    fprintf(stderr, "STACK TRACE: (%zd frames)\n", nframes);
+    for (i = 0; i < nframes; i++)
+        fprintf(stderr, "    %s\n", bt_strings[i]);
+
+    free(bt_strings);
+#endif
+}
