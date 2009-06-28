fbdae3290fdb46e5e6d939c781813ad27ddd68a0
neilc@samurai.com <none@none>
2009-06-28 16:34:30 -0700

    Checkpoint.

--- src/libcol/planner/installer.c
+++ src/libcol/planner/installer.c
@@ -1,10 +1,44 @@
 #include "col-internal.h"
+#include "router.h"
 #include "planner/installer.h"
 
-void
-install_plan(ProgramPlan *plan, ColInstance *col)
+static void
+plan_install_defines(ProgramPlan *plan, ColInstance *col)
 {
     ;
 }
 
+static void
+plan_install_rules(ProgramPlan *plan, ColInstance *col)
+{
+    ;
+}
 
+static Tuple *
+fact_make_tuple(AstFact *fact, ColInstance *col)
+{
+    return NULL;
+}
+
+static void
+plan_install_facts(ProgramPlan *plan, ColInstance *col)
+{
+    ListCell *lc;
+
+    foreach (lc, plan->facts)
+    {
+        AstFact *fact = (AstFact *) lc_ptr(lc);
+        Tuple *t;
+
+        t = fact_make_tuple(fact, col);
+        router_enqueue(col->router, t);
+    }
+}
+
+void
+install_plan(ProgramPlan *plan, ColInstance *col)
+{
+    plan_install_defines(plan, col);
+    plan_install_rules(plan, col);
+    plan_install_facts(plan, col);
+}
