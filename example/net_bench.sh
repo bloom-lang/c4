99c8544fb63252b6b381af06c2ddc839b3eb53f1
Neil Conway <nrc@cs.berkeley.edu>
2009-09-18 22:59:21 -0700

    Initial net bench test harness, and associated (trivial) Overlog program.

---
+++ example/net_bench.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+COL=$HOME/build-col/src/cli/coverlog
+
+PORT1=11000
+PORT2=11001
+
+LOCALHOST="W88256X9YJX.local"
+SELF1="tcp:$LOCALHOST:$PORT1"
+SELF2="tcp:$LOCALHOST:$PORT2"
+
+$COL -p $PORT1 ./net_loop.olg &
+
+$COL -p $PORT2 -s "ping(\"$SELF2\", \"$SELF1\");" ./net_loop.olg
