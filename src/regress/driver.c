591866a6b423554d5b5f4166eb94a6a420a4b4a3
Neil Conway <nrc@cs.berkeley.edu>
2010-01-03 18:15:36 -0800

    Checkpoint some initial work on the regression test driver.

--- src/regress/driver.c
+++ src/regress/driver.c
@@ -2,6 +2,15 @@
 
 #include "c4-api.h"
 
+#define INPUT_FILE_DIR "/Users/neilconway/build_c4/src/regress/input"
+#define EXPECTED_FILE_DIR "/Users/neilconway/build_c4/src/regress/expected"
+
+static char *test_files[] = {
+    "hello, world",
+    "bar",
+    "baz"
+};
+
 int
 main(void)
 {
@@ -10,6 +19,7 @@ main(void)
     c4_initialize();
 
     c4 = c4_make(0);
+    c4_start(c4);
     c4_destroy(c4);
 
     c4_terminate();
