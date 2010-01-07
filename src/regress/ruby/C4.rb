39a5556f0721510969fe8101a84f634c124f1c22
Neil Conway <nrc@cs.berkeley.edu>
2010-01-07 13:46:28 -0800

    Rename Col => C4Lib.

--- src/regress/ruby/C4.rb
+++ src/regress/ruby/C4.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'ffi'
 
 class C4
-  module Col
+  module C4Lib
     extend FFI::Library
     ffi_lib 'c4'
     attach_function 'c4_initialize', [], :void
@@ -15,26 +15,26 @@ class C4
   end
 
   def initialize(port=0)
-    Col.c4_initialize
-    @c4 = Col.c4_make(port)
+    C4Lib.c4_initialize
+    @c4 = C4Lib.c4_make(port)
   end
 
   # TODO: check status
   def install_prog(inprog)
-    s = Col.c4_install_file(@c4, inprog)
+    s = C4Lib.c4_install_file(@c4, inprog)
   end
 
   # TODO: check status
   def install_str(inprog)
-    s = Col.c4_install_str(@c4, inprog)
+    s = C4Lib.c4_install_str(@c4, inprog)
   end
   
   def dump_table(tbl_name)
-    Col.c4_dump_table(@c4, tbl_name)
+    C4Lib.c4_dump_table(@c4, tbl_name)
   end
 
   def finish
-    Col.c4_destroy(@c4)
-    Col.c4_terminate
+    C4Lib.c4_destroy(@c4)
+    C4Lib.c4_terminate
   end
 end
