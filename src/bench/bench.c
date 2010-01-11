1179f9f81d886c4fbf9f88f0578a014252ccda85
Neil Conway <nrc@cs.berkeley.edu>
2010-01-10 22:34:24 -0800

    Initial code for network bench program.

--- src/bench/bench.c
+++ src/bench/bench.c
@@ -1,12 +1,23 @@
 #include <apr_general.h>
 #include <apr_getopt.h>
+#include <apr_strings.h>
+#include <apr_thread_cond.h>
+#include <apr_thread_mutex.h>
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "c4-api.h"
 
+#define NET_BENCH_SRC "/Users/neilconway/c4/example/net_loop.olg"
+
 static void usage(void);
+static void do_net_bench(apr_pool_t *pool);
+static void do_perf_bench(apr_pool_t *pool);
+static void net_done_cb(struct Tuple *tuple, struct TableDef *tbl_def,
+                        void *data);
+static void net_install_program(C4Client *c);
 
 int
 main(int argc, const char *argv[])
@@ -46,6 +57,12 @@ main(int argc, const char *argv[])
     if (s != APR_EOF)
         usage();
 
+    if (net_bench)
+        do_net_bench(pool);
+    else
+        do_perf_bench(pool);
+
+    apr_pool_destroy(pool);
     c4_terminate();
     return 0;
 }
@@ -53,6 +70,74 @@ main(int argc, const char *argv[])
 static void
 usage(void)
 {
-    ;
+    printf("Usage: bench [ -n ]\n");
+    exit(1);
 }
 
+typedef struct NetCallbackData
+{
+    bool is_done;
+    apr_thread_mutex_t *lock;
+    apr_thread_cond_t *cond;
+} NetCallbackData;
+
+static void
+do_net_bench(apr_pool_t *pool)
+{
+    C4Client *c1;
+    C4Client *c2;
+    NetCallbackData cb_data;
+    char *ping_fact;
+
+    c1 = c4_make(0);
+    c2 = c4_make(0);
+
+    net_install_program(c1);
+    net_install_program(c2);
+
+    cb_data.is_done = false;
+    (void) apr_thread_mutex_create(&cb_data.lock, APR_THREAD_MUTEX_DEFAULT, pool);
+    (void) apr_thread_cond_create(&cb_data.cond, pool);
+    c4_register_callback(c1, "done", net_done_cb, &cb_data);
+
+    ping_fact = apr_psprintf(pool, "ping(\"tcp:localhost:%d\", \"tcp:localhost:%d\", 0);",
+                             c4_get_port(c1), c4_get_port(c2));
+
+    c4_install_str(c1, ping_fact);
+
+    do {
+        apr_thread_cond_wait(cb_data.cond, cb_data.lock);
+    } while (!cb_data.is_done);
+
+    apr_thread_mutex_unlock(cb_data.lock);
+    c4_destroy(c1);
+    c4_destroy(c2);
+}
+
+static void
+net_done_cb(struct Tuple *tuple, struct TableDef *tbl_def, void *data)
+{
+    NetCallbackData *cb_data = (NetCallbackData *) data;
+
+    printf("%s invoked!\n", __func__);
+
+    apr_thread_mutex_lock(cb_data->lock);
+    cb_data->is_done = true;
+    apr_thread_cond_signal(cb_data->cond);
+    apr_thread_mutex_unlock(cb_data->lock);
+}
+
+static void
+net_install_program(C4Client *c)
+{
+    c4_install_str(c, "define(ping, keys(0), {@string, string, int8});");
+    c4_install_str(c, "define(done, keys(0), {int8});");
+    c4_install_str(c, "ping(X, Y, C + 1) :- ping(Y, X, C), C < 100000;");
+    c4_install_str(c, "done(C) :- ping(_, _, C), C >= 100000;");
+}
+
+static void
+do_perf_bench(apr_pool_t *pool)
+{
+    ;
+}
