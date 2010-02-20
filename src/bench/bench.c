a6619fd578170e66d62730a8583275f0326cfe8b
Neil Conway <nrc@cs.berkeley.edu>
2010-02-19 20:28:47 -0800

    Add a simple join perf test.

--- src/bench/bench.c
+++ src/bench/bench.c
@@ -10,67 +10,7 @@
 #include "c4-api.h"
 #include "util/thread_sync.h"
 
-static void usage(void);
-static void do_net_bench(apr_pool_t *pool);
-static void do_perf_bench(apr_pool_t *pool);
-
-int
-main(int argc, const char *argv[])
-{
-    static const apr_getopt_option_t opt_option[] =
-        {
-            {"net", 'n', false, "network benchmark"},
-            { NULL, 0, 0, NULL }
-        };
-    apr_pool_t *pool;
-    apr_getopt_t *opt;
-    int optch;
-    const char *optarg;
-    apr_status_t s;
-    bool net_bench = false;
-    apr_time_t start_time;
-
-    c4_initialize();
-
-    (void) apr_pool_create(&pool, NULL);
-    (void) apr_getopt_init(&opt, pool, argc, argv);
-
-    while ((s = apr_getopt_long(opt, opt_option,
-                                &optch, &optarg)) == APR_SUCCESS)
-    {
-        switch (optch)
-        {
-            case 'n':
-                net_bench = true;
-                break;
-
-            default:
-                printf("Unrecognized option: %c\n", optch);
-                usage();
-        }
-    }
-
-    if (s != APR_EOF)
-        usage();
-
-    start_time = apr_time_now();
-
-    if (net_bench)
-        do_net_bench(pool);
-    else
-        do_perf_bench(pool);
-
-    printf("Benchmark duration: %" APR_TIME_T_FMT " usec\n",
-           (apr_time_now() - start_time));
-
-    start_time = apr_time_now();
-    apr_pool_destroy(pool);
-    printf("Shutdown duration: %" APR_TIME_T_FMT " usec\n",
-           (apr_time_now() - start_time));
-
-    c4_terminate();
-    return 0;
-}
+typedef void (*program_install_f)(C4Client *c);
 
 static void
 usage(void)
@@ -132,11 +72,86 @@ perf_install_program(C4Client *c)
 }
 
 static void
-do_perf_bench(apr_pool_t *pool)
+join_install_program(C4Client *c)
+{
+    c4_install_str(c, "define(t, keys(0), {int8});");
+    c4_install_str(c, "define(s, keys(0), {int8});");
+    c4_install_str(c, "s(0);");
+    c4_install_str(c, "t(A + 1) :- t(A), s(B), A >= B, A < 3000000;");
+}
+
+static void
+do_simple_bench(program_install_f prog, apr_pool_t *pool)
 {
     C4Client *c;
 
     c = c4_make(pool, 0);
-    perf_install_program(c);
+    (*prog)(c);
     c4_install_str(c, "t(0);");
+}
+
+int
+main(int argc, const char *argv[])
+{
+    static const apr_getopt_option_t opt_option[] =
+        {
+            {"join", 'j', false, "join benchmark"},
+            {"net", 'n', false, "network benchmark"},
+            { NULL, 0, 0, NULL }
+        };
+    apr_pool_t *pool;
+    apr_getopt_t *opt;
+    int optch;
+    const char *optarg;
+    apr_status_t s;
+    bool join_bench = false;
+    bool net_bench = false;
+    apr_time_t start_time;
+
+    c4_initialize();
+
+    (void) apr_pool_create(&pool, NULL);
+    (void) apr_getopt_init(&opt, pool, argc, argv);
+
+    while ((s = apr_getopt_long(opt, opt_option,
+                                &optch, &optarg)) == APR_SUCCESS)
+    {
+        switch (optch)
+        {
+            case 'j':
+                join_bench = true;
+                break;
+
+            case 'n':
+                net_bench = true;
+                break;
+
+            default:
+                printf("Unrecognized option: %c\n", optch);
+                usage();
+        }
+    }
+
+    if (s != APR_EOF || (join_bench && net_bench))
+        usage();
+
+    start_time = apr_time_now();
+
+    if (join_bench)
+        do_simple_bench(join_install_program, pool);
+    else if (net_bench)
+        do_net_bench(pool);
+    else
+        do_simple_bench(perf_install_program, pool);
+
+    printf("Benchmark duration: %" APR_TIME_T_FMT " usec\n",
+           (apr_time_now() - start_time));
+
+    start_time = apr_time_now();
+    apr_pool_destroy(pool);
+    printf("Shutdown duration: %" APR_TIME_T_FMT " usec\n",
+           (apr_time_now() - start_time));
+
+    c4_terminate();
+    return 0;
 }
