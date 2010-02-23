5e2040e8383355071e0ec6f310eaa84315a35609
Neil Conway <nrc@cs.berkeley.edu>
2010-02-22 18:19:56 -0800

    Cleanup regression test script. Remove pointless dependency on Test::Unit.

--- src/regress/regression.rb
+++ src/regress/regression.rb
@@ -1,50 +1,45 @@
 require 'rubygems'
 require 'C4'
-require 'test/unit'
 
-class TestTuple < Test::Unit::TestCase
-  INPUT_DIR="input"
-  OUTPUT_DIR="output"
-  EXPECTED_DIR="expected"
+INPUT_DIR="input"
+OUTPUT_DIR="output"
+EXPECTED_DIR="expected"
 
-  def setup
-    @c4 = C4.new
-  end
-
-  def teardown
-    @c4.destroy
-  end
-
-  def test_loop
-    tests = Dir.entries(INPUT_DIR).reject { |i| i.match(/^\./) }
-    tests.each do |test|
-      print "Running test \"#{test}\"..."
-      input = ""
-      output = ""
-      File.open("#{INPUT_DIR}/#{test}").each_line do |line|
-        if line =~ /^\\dump (.+)/
-          @c4.install_str(input) unless input == ""
-          output << "**** \\dump \"#{$1}\" ****\n"
-          output << @c4.dump_table($1).split("\n").sort.join("\n")
-          output << "\n"
-          input = ""
-        else
-          input << line
-        end
-      end
-      puts " done"
-
-      Dir.mkdir(OUTPUT_DIR) unless File.directory?(OUTPUT_DIR)
-      File.open("#{OUTPUT_DIR}/#{test}", 'w') do |f|
-        f.write(output)
+def do_test(c4)
+  puts "===="
+  tests = Dir.entries(INPUT_DIR).reject { |i| i.match(/^\./) }
+  tests.each do |test|
+    print "Running test \"#{test}\"..."
+    input = ""
+    output = ""
+    File.open("#{INPUT_DIR}/#{test}").each_line do |line|
+      if line =~ /^\\dump (.+)/
+        c4.install_str(input) unless input == ""
+        output << "**** \\dump \"#{$1}\" ****\n"
+        output << c4.dump_table($1).split("\n").sort.join("\n")
+        output << "\n"
+        input = ""
+      else
+        input << line
       end
     end
+    puts " done"
 
-    `diff -ur #{EXPECTED_DIR} #{OUTPUT_DIR} > regress.diffs`
-    if $? == 0
-      puts "All tests passed!"
-    else
-      puts "Tests failed; see regress.diffs"
+    Dir.mkdir(OUTPUT_DIR) unless File.directory?(OUTPUT_DIR)
+    File.open("#{OUTPUT_DIR}/#{test}", 'w') do |f|
+      f.write(output)
     end
+  end
+
+  puts "===="
+  `diff -ur #{EXPECTED_DIR} #{OUTPUT_DIR} > regress.diffs`
+  if $? == 0
+    puts "All tests passed!"
+  else
+    puts "TESTS FAILED; see regress.diffs"
   end
 end
+
+c = C4.new
+do_test(c)
+c.destroy
